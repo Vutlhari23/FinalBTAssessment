0dedf1ab91e8050f2a79e02cab9c9604552c2a40
Vutlhari Maswanganyi <164008799+Vutlhari23@users.noreply.github.com>
2026-06-09 19:45:48 +0000

    adding files

---
+++ function.js
@@ -0,0 +1,54 @@
+
+
+function validateEmail(){
+
+    let email = document.getElementById("email").value;
+
+    let atSign = 0;
+    let dotSign = 0;
+
+    for(let i = 0; i < email.length; i++){
+
+        if(email[i] === "@"){
+            atSign++;
+        }
+
+        if(email[i] === "."){
+            dotSign++;
+        }
+    }
+
+    if(atSign == 1 && dotSign >= 1){
+
+        document.getElementById("email").style.color = "green";
+
+        localStorage.setItem("email", email);
+
+        window.location.href = "thank-u.html";
+
+    }else{
+
+        document.getElementById("email").style.color = "red";
+        document.getElementById("email").style.backgroundColor = "pink";
+        document.getElementById("err").innerHTML = "Please provide a valid email";            
+    }
+}
+
+function resized(){
+
+    const width = window.innerWidth;
+
+    if(width < 1500){
+
+        document.getElementById('main').style.display = 'none';
+        document.getElementById('main1').style.display = 'block';
+
+    }else{
+
+        document.getElementById('main').style.display = 'flex';
+        document.getElementById('main1').style.display = 'none';
+    }
+}
+
+window.addEventListener('resize', resized);
+window.addEventListener('load', resized);
