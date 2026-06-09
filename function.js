6425fd5f40563ea3d36aa268feeab2d690413ce6
Vutlhari Maswanganyi <164008799+Vutlhari23@users.noreply.github.com>
2026-06-09 19:53:14 +0000

    Modified files

--- function.js
+++ function.js
@@ -33,6 +33,39 @@ function validateEmail(){
         document.getElementById("err").innerHTML = "Please provide a valid email";            
     }
 }
+function validateEmailOne(){
+
+    let email = document.getElementById("email").value;
+
+    let atSign = 0;
+    let dotSign = 0;
+
+    for(let i = 0; i < email.length; i++){
+
+        if(email[i] === "@"){
+            atSign++;
+        }
+
+        if(email[i] === "."){
+            dotSign++;
+        }
+    }
+
+    if(atSign == 1 && dotSign >= 1){
+
+        document.getElementById("email").style.color = "green";
+
+        localStorage.setItem("email", email);
+
+        window.location.href = "thank-u.html";
+
+    }else{
+
+        document.getElementById("email").style.color = "red";
+        document.getElementById("email").style.backgroundColor = "pink";
+        document.getElementById("err").innerHTML = "Please provide a valid email";            
+    }
+}
 
 function resized(){
 
